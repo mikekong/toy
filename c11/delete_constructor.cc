299272f5917fcf66c365d8c59a1fd1bb373dbeed
Mikekong <diy286@gmail.com>
2021-03-05 00:44:46 +0800

    Squashed commit of the following:
    commit b0a8eebcdcc3e3cb4c245cfe75731dbc3b6d6142
    Author: Mikekong <diy286@gmail.com>
    Date:   Fri Mar 5 00:44:23 2021 +0800
    
        hash & map with move-constructor

--- c11/delete_constructor.cc
+++ c11/delete_constructor.cc
@@ -4,12 +4,16 @@
 #include <vector>
 #include <mutex>
 #include <memory>
+#include <string>
+#include <map>
+#include <unordered_map>
 
 
 using namespace std;
 
+template <class VType>
 struct AC {
-    AC(int v) : v(v) {}
+    AC(VType v) : v(v) {}
     AC(const AC &) = delete;
     AC(const AC &&) = delete;
     AC & operator=(const AC &) = delete;
@@ -17,27 +21,53 @@ struct AC {
     ~AC() {
         cout << "AC: " << v << " destructor" << endl;
     }
-    int v{0};
+    VType v{0};
     mutex m;
 };
 
-void f1(vector<int> vs) {
-    vector<AC> vt;
+void vector_f(vector<int> vs) {
+    vector<AC<int>> vt;
     for (unsigned i = 0; i < vs.size(); ++i) {
+        //  Invalid as vector will use move constructor when resize
         //vt.emplace_back(i);
     }
     if (vs.size() <= 0) return ;
 
-    unique_ptr<AC> varr[vs.size()];
+    unique_ptr<AC<int>> varr[vs.size()];
 
     for (unsigned i = 0; i < vs.size(); ++i) {
-        varr[i] = make_unique<AC>(i); 
+        varr[i] = make_unique<AC<int>>(i); 
     }
 }
 
+void hash_f(vector<string> vs) {
+    unordered_map<string, unique_ptr<AC<string>>> name2inst;
+
+    for (auto & n : vs) {
+        name2inst[n] = make_unique<AC<string>>(n);
+    }
+
+    for (auto & it : name2inst) {
+        cout << it.first << "->" << it.second->v << endl;
+    }
+}
+
+void rbtree_f(vector<string> vs) {
+    map<string, unique_ptr<AC<string>>> name2inst;
+
+    for (auto & n : vs) {
+        name2inst[n] = make_unique<AC<string>>(n);
+    }
+
+    for (auto & it : name2inst) {
+        cout << it.first << "->" << it.second->v << endl;
+    }
+}
 
 int main() {
-    AC a(2);
-    f1(vector<int>{1,2,3,4});
+    AC<int> a(2);
+    vector_f(vector<int>{1,2,3,4});
+    hash_f(vector<string>{"a1", "b22", "c333", "d4444", "e55555"});
+    rbtree_f(vector<string>{"e55555", "b22", "c333", "d4444", "a1"});
     return 0;
 }
